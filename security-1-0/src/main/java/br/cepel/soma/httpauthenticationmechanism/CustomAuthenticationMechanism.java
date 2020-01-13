d7420865dafa9adf9658d36aa6c53647c72089a3
Joao Antonio Ferreira <joao.parana@gmail.com>
2020-01-13 16:42:35 -0300

    Ajustando CustomAuthenticationMechanism

--- security-1-0/src/main/java/br/cepel/soma/httpauthenticationmechanism/CustomAuthenticationMechanism.java
+++ security-1-0/src/main/java/br/cepel/soma/httpauthenticationmechanism/CustomAuthenticationMechanism.java
@@ -6,9 +6,12 @@ import javax.security.enterprise.AuthenticationStatus;
 import javax.security.enterprise.authentication.mechanism.http.HttpAuthenticationMechanism;
 import javax.security.enterprise.authentication.mechanism.http.HttpMessageContext;
 import javax.security.enterprise.authentication.mechanism.http.RememberMe;
+import javax.security.enterprise.identitystore.CredentialValidationResult;
 import javax.security.enterprise.identitystore.IdentityStoreHandler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+import java.util.TreeSet;
 
 @RememberMe(
         cookieMaxAgeSeconds = 3600
@@ -21,7 +24,21 @@ public class CustomAuthenticationMechanism implements HttpAuthenticationMechanis
 
     @Override
     public AuthenticationStatus validateRequest(HttpServletRequest req, HttpServletResponse res, HttpMessageContext msg) {
-        return msg.doNothing();
+
+        String username = ""; // ...get username from HttpRequest...
+        String password = ""; //...get password from HttpRequest...
+
+        // CredentialValidationResult result = idStoreHandler.validate(
+        //      new UsernamePasswordCredential(username, password));
+        Set<String> groups = new TreeSet<String>();
+        groups.add("admin");
+        groups.add("editor");
+        groups.add("reader");
+        groups.add("guest");
+        CredentialValidationResult result = new CredentialValidationResult("pedro", groups);
+
+        return msg.notifyContainerAboutLogin(result);
+        // return msg.doNothing();
     }
 
 }
